4b733207f2e8885fe73d660a93cd74862318543d
Ray Clark <raynichc@users.noreply.github.com>
2021-02-07 13:34:04 +1100

    Fixed membership retrieval

--- controllers/Membership.js
+++ controllers/Membership.js
@@ -8,7 +8,7 @@ const sanitizeUser = user =>
     model: strapi.query('user', 'users-permissions').model,
   });
 
-const stripe = require("stripe")(""); //TODO: Get api key from config?
+//const stripe = require("stripe")(""); //TODO: Get api key from config?
 
 module.exports = {
   
@@ -19,18 +19,16 @@ module.exports = {
   async pay(ctx) {
     const { membershipID, token } = ctx.request.body;
 
-    //Get membership details from CMS
-    if (!membershipID.match(/^[0-9a-fA-F]{24}$/)) {
-      return ctx.notFound();
-    }
-
-    const result = await strapi.query('membership').findOne({ id: membershipID });
-    const membership = _.first(result) || null;
+    //Get membership detailsfrom CMS
+    const membership = await strapi.query('membership').findOne({ id: membershipID });
 
     if (membership == null) {
-      return ctx.notFound();
+      return ctx.badRequest('membership.notFound');
     }
 
+    return membership;
+
+    /*
     const charge = await stripe.charges.create({
       amount: membership.price,
       currency: "aud",
@@ -46,5 +44,6 @@ module.exports = {
     //TODO: Update user membership details if successful
 
     //TODO: Return success
+    */
   },
 };
